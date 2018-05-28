96b80888d6622871455dbecf15d541ca8232d644
Jason Welsh <jasonbwelsh@gmail.com>
2018-05-28 17:17:31 -0400

    Fade in animation added and setTimeout function to remove fade in class on click after .5s

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
 const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.4;
 
-imgs.forEach(img => img.addEventListener('click', imgClick)
-);
+imgs.forEach(img => img.addEventListener('click', imgClick));
 
 function imgClick(e) {
+  // Reset imgs opacity before style applied
+  imgs.forEach(img => img.style.opacity = 1);
+  // Change current image to source of clicked image
   current.src = e.target.src;
+
+  // Add .fade-in class to current
+  current.classList.add('fade-in');
+
+  // Remove .fade-in class
+  setTimeout(() => {
+    current.classList.remove('fade-in');
+  }, 500);
+
+  // Change the opacity to opacity var
+  e.target.style.opacity = opacity;
+
 }
