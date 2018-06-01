278d134ad69535f1d220d9697992585f7a75f433
Jason Welsh <jasonbwelsh@gmail.com>
2018-06-01 13:03:55 -0400

    .imgs img:hover state added

--- scripts.js
+++ scripts.js
@@ -11,23 +11,17 @@ imgs[0].style.opacity = opacity;
 function imgClick(e) {
   // Reset imgs opacity before style applied
   imgs.forEach(img => img.style.opacity = 1);
-
   // Change current image to source of clicked image
   current.src = e.target.src;
-
   // Add .fade-in class to current
   current.classList.add('fade-in');
-
   // Remove .fade-in class
   setTimeout(() => {
     current.classList.remove('fade-in');
   }, 500);
-
   // Change the opacity to opacity var
   e.target.style.opacity = opacity;
-
 }
-
 
 // refactor to use event delegation
 
