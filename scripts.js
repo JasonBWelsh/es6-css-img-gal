ab2506ae06b34f5497123eea743883e958bc5615
Jason Welsh <jasonbwelsh@gmail.com>
2018-06-01 12:28:07 -0400

    explicit grid defined for template rows, object-fit property with overflow and width height set to keep .imgs img from changing grid dimensions. Images now load equally regardless of size or aspect ratio

--- scripts.js
+++ scripts.js
@@ -2,11 +2,15 @@ const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.imgs img');
 const opacity = 0.4;
 
+// Set first img opacity
+imgs[0].style.opacity = opacity;
+
 imgs.forEach(img => img.addEventListener('click', imgClick));
 
 function imgClick(e) {
   // Reset imgs opacity before style applied
   imgs.forEach(img => img.style.opacity = 1);
+
   // Change current image to source of clicked image
   current.src = e.target.src;
 
